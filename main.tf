8509ddf62ec4701aa2afd43ef2beb01e8d23c042
toshiyana0306@gmail.com <yanagimoto-toshiki@dmm.com>
2023-06-26 16:17:46 +0900

    add ALB

--- main.tf
+++ main.tf
@@ -238,3 +238,67 @@ resource "aws_route_table_association" "private_1d" {
   subnet_id      = "${aws_subnet.private_1d.id}"
   route_table_id = "${aws_route_table.private_1d.id}"
 }
+
+# Security Group
+resource "aws_security_group" "alb" {
+  name = "handson-alb"
+  description = "handson alb"
+  vpc_id = "${aws_vpc.main.id}"
+
+  # Security Group内のリソースからInternetへのアクセスを許可
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "handson-alb"
+  }
+}
+
+# Security Group Rule
+resource "aws_security_group_rule" "alb_http" {
+  security_group_id = "${aws_security_group.alb.id}"
+
+  # InternetからSecurity Group内のリソースへのアクセスを許可
+  type = "ingress"
+
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# ALB
+resource "aws_lb" "main" {
+  load_balancer_type = "application"
+  name = "handson"
+
+  security_groups = ["${aws_security_group.alb.id}"]
+  subnets = ["${aws_subnet.public_1a.id}", "${aws_subnet.public_1c.id}", "${aws_subnet.public_1d.id}"]
+}
+
+# LB Listener
+resource "aws_lb_listener" "main" {
+  # HTTPでのアクセスを受け付ける
+  port              = "80"
+  protocol          = "HTTP"
+
+  # ALBのarnを指定
+  # (arnはAmazon Resource Names の略で、その名の通りリソースを特定するための一意な名前(id).)
+  load_balancer_arn = "${aws_lb.main.arn}"
+
+  # "ok"という固定レスポンスを設定
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      status_code = "200"
+      message_body = "ok"
+    }
+  }
+}
